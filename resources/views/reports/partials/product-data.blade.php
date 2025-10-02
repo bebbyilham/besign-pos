18896ed94c81537de05504e355c52c6185548724
Bebby Ilham Akbar Aresta <bebbyilham@gmail.com>
2025-10-02 08:46:29 +0700

    product report

--- resources/views/reports/partials/product-data.blade.php
+++ resources/views/reports/partials/product-data.blade.php
@@ -1,103 +1,95 @@
 <div class="max-w-full">
-  <div class="text-center space-y-2">
+  <div class="text-center space-y-2 mb-4">
     <h1 class="text-3xl font-semibold">{{ __('Product Report') }}</h1>
     <h3 class="text-xl">{{ $header['shop_name'] }}</h3>
+    <p>
+      {{ __('Period') }}: 
+      <b>{{ $header['start_date'] }} - {{ $header['end_date'] }}</b>
+    </p>
   </div>
 
-  <p class="mb-4">
-    {{ __('Period') }}:
-    <b>{{ $header['start_date'] }} - {{ $header['end_date'] }}</b>
-  </p>
-
   {{-- TABLE DETAIL PRODUK --}}
-  <x-table class="w-full table-fixed border-collapse border text-xs
-    [&_th]:whitespace-normal [&_th]:break-words [&_th]:align-top [&_th]:px-2 [&_th]:py-1
-    [&_td]:whitespace-normal [&_td]:break-words [&_td]:overflow-hidden [&_td]:align-top [&_td]:px-2 [&_td]:py-1">
-
-    <x-table-header>
-      <x-table-row>
-        <x-table-header-cell class="w-[90px]">SKU</x-table-header-cell>
-        <x-table-header-cell class="w-[220px]">Product Name</x-table-header-cell>
-        <x-table-header-cell class="w-[110px] text-right">Initial Price</x-table-header-cell>
-        <x-table-header-cell class="w-[100px] text-right">Price</x-table-header-cell>
-        <x-table-header-cell class="w-[80px] text-right">Qty</x-table-header-cell>
-        <x-table-header-cell class="w-[110px] text-right">Pembelian</x-table-header-cell>
-        <x-table-header-cell class="w-[110px] text-right">Selling</x-table-header-cell>
-        <x-table-header-cell class="w-[100px] text-right">Discount</x-table-header-cell>
-        <x-table-header-cell class="w-[100px] text-right">Cost</x-table-header-cell>
-        <x-table-header-cell class="w-[120px] text-right">Net Selling</x-table-header-cell>
-        <x-table-header-cell class="w-[120px] text-right">Gross Profit</x-table-header-cell>
-        <x-table-header-cell class="w-[120px] text-right">Net Profit</x-table-header-cell>
-        <x-table-header-cell class="w-[100px] text-right">Stok Akhir</x-table-header-cell>
-        <x-table-header-cell class="w-[140px] text-right">Saldo Stok Akhir</x-table-header-cell>
-      </x-table-row>
-    </x-table-header>
-
+  <table class="w-full table-fixed border-collapse border text-xs">
+    <thead class="bg-gray-100">
+      <tr>
+        <th class="w-[80px] px-2 py-1 border break-words whitespace-normal">SKU</th>
+        <th class="w-[200px] px-2 py-1 border break-words whitespace-normal">Product Name</th>
+        <th class="w-[100px] px-2 py-1 border text-right">Initial Price</th>
+        <th class="w-[100px] px-2 py-1 border text-right">Price</th>
+        <th class="w-[80px] px-2 py-1 border text-right">Qty</th>
+        <th class="w-[100px] px-2 py-1 border text-right">Pembelian</th>
+        <th class="w-[100px] px-2 py-1 border text-right">Selling</th>
+        <th class="w-[100px] px-2 py-1 border text-right">Discount</th>
+        <th class="w-[100px] px-2 py-1 border text-right">Cost</th>
+        <th class="w-[120px] px-2 py-1 border text-right">Net Selling</th>
+        <th class="w-[120px] px-2 py-1 border text-right">Gross Profit</th>
+        <th class="w-[120px] px-2 py-1 border text-right">Net Profit</th>
+        <th class="w-[100px] px-2 py-1 border text-right">Stok Akhir</th>
+        <th class="w-[140px] px-2 py-1 border text-right">Saldo Stok Akhir</th>
+      </tr>
+    </thead>
     <tbody>
       @foreach($reports as $report)
-        <x-table-row>
-          <x-table-cell class="w-[90px] max-w-[90px] break-words">{{ $report['sku'] }}</x-table-cell>
-          <x-table-cell class="w-[220px] max-w-[220px] break-words">{{ $report['name'] }}</x-table-cell>
-          <x-table-cell class="w-[110px] text-right">{{ $report['initial_price'] }}</x-table-cell>
-          <x-table-cell class="w-[100px] text-right">{{ $report['selling_price'] }}</x-table-cell>
-          <x-table-cell class="w-[80px] text-right">{{ $report['qty'] }}</x-table-cell>
-          <x-table-cell class="w-[110px] text-right">{{ $report['purchase_qty'] }}</x-table-cell>
-          <x-table-cell class="w-[110px] text-right">{{ $report['selling'] }}</x-table-cell>
-          <x-table-cell class="w-[100px] text-right">{{ $report['discount_price'] }}</x-table-cell>
-          <x-table-cell class="w-[100px] text-right">{{ $report['cost'] }}</x-table-cell>
-          <x-table-cell class="w-[120px] text-right">{{ $report['total_after_discount'] }}</x-table-cell>
-          <x-table-cell class="w-[120px] text-right">{{ $report['gross_profit'] }}</x-table-cell>
-          <x-table-cell class="w-[120px] text-right">{{ $report['net_profit'] }}</x-table-cell>
-          <x-table-cell class="w-[100px] text-right">{{ $report['ending_stock'] }}</x-table-cell>
-          <x-table-cell class="w-[140px] text-right">{{ $report['ending_stock_balance'] }}</x-table-cell>
-        </x-table-row>
+      <tr>
+        <td class="w-[80px] max-w-[80px] px-2 py-1 border break-words whitespace-normal">{{ $report['sku'] }}</td>
+        <td class="w-[200px] max-w-[200px] px-2 py-1 border break-words whitespace-normal">{{ $report['name'] }}</td>
+        <td class="w-[100px] px-2 py-1 border text-right">{{ $report['initial_price'] }}</td>
+        <td class="w-[100px] px-2 py-1 border text-right">{{ $report['selling_price'] }}</td>
+        <td class="w-[80px] px-2 py-1 border text-right">{{ $report['qty'] }}</td>
+        <td class="w-[100px] px-2 py-1 border text-right">{{ $report['purchase_qty'] }}</td>
+        <td class="w-[100px] px-2 py-1 border text-right">{{ $report['selling'] }}</td>
+        <td class="w-[100px] px-2 py-1 border text-right">{{ $report['discount_price'] }}</td>
+        <td class="w-[100px] px-2 py-1 border text-right">{{ $report['cost'] }}</td>
+        <td class="w-[120px] px-2 py-1 border text-right">{{ $report['total_after_discount'] }}</td>
+        <td class="w-[120px] px-2 py-1 border text-right">{{ $report['gross_profit'] }}</td>
+        <td class="w-[120px] px-2 py-1 border text-right">{{ $report['net_profit'] }}</td>
+        <td class="w-[100px] px-2 py-1 border text-right">{{ $report['ending_stock'] }}</td>
+        <td class="w-[140px] px-2 py-1 border text-right">{{ $report['ending_stock_balance'] }}</td>
+      </tr>
       @endforeach
 
       {{-- ROW TOTAL --}}
-      <x-table-row class="font-semibold bg-gray-50">
-        <x-table-cell colspan="4" class="text-right">Total</x-table-cell>
-        <x-table-cell class="text-right">{{ $footer['total_qty'] }}</x-table-cell>
-        <x-table-cell class="text-right">{{ $footer['total_gross'] }}</x-table-cell>
-        <x-table-cell class="text-right">{{ $footer['total_discount'] }}</x-table-cell>
-        <x-table-cell colspan="2"></x-table-cell>
-        <x-table-cell class="text-right">{{ $footer['total_gross_profit'] }}</x-table-cell>
-        <x-table-cell class="text-right">{{ $footer['total_net_profit_before_discount_selling'] }}</x-table-cell>
-        <x-table-cell colspan="2"></x-table-cell>
-      </x-table-row>
+      <tr class="font-semibold bg-gray-50">
+        <td colspan="4" class="px-2 py-1 border text-right">Total</td>
+        <td class="px-2 py-1 border text-right">{{ $footer['total_qty'] }}</td>
+        <td class="px-2 py-1 border text-right">{{ $footer['total_gross'] }}</td>
+        <td class="px-2 py-1 border text-right">{{ $footer['total_discount'] }}</td>
+        <td colspan="2" class="border"></td>
+        <td class="px-2 py-1 border text-right">{{ $footer['total_gross_profit'] }}</td>
+        <td class="px-2 py-1 border text-right">{{ $footer['total_net_profit_before_discount_selling'] }}</td>
+        <td colspan="2" class="border"></td>
+      </tr>
     </tbody>
-  </x-table>
+  </table>
 
   {{-- TABLE GRAND TOTAL --}}
-  <x-table class="w-full table-fixed mt-4 border text-xs
-    [&_td]:whitespace-normal [&_td]:break-words [&_td]:overflow-hidden [&_td]:align-top [&_td]:p-2">
-    <x-table-header>
-      <x-table-row>
-        <x-table-header-cell colspan="8" class="text-center">
-          {{ __('Grand Total') }}
-        </x-table-header-cell>
-      </x-table-row>
-      <x-table-row>
-        <x-table-header-cell class="w-[150px]">Total Saldo Stok</x-table-header-cell>
-        <x-table-header-cell class="w-[120px]">Total Biaya</x-table-header-cell>
-        <x-table-header-cell class="w-[120px]">Penjualan</x-table-header-cell>
-        <x-table-header-cell class="w-[140px]">Discount per Penjualan</x-table-header-cell>
-        <x-table-header-cell class="w-[160px]">Penjualan Setelah Discount</x-table-header-cell>
-        <x-table-header-cell class="w-[140px]">Keuntungan Kotor</x-table-header-cell>
-        <x-table-header-cell class="w-[180px]">Keuntungan Bersih Sebelum Diskon</x-table-header-cell>
-        <x-table-header-cell class="w-[180px]">Keuntungan Bersih Setelah Diskon</x-table-header-cell>
-      </x-table-row>
-    </x-table-header>
+  <table class="w-full table-fixed mt-4 border-collapse border text-xs">
+    <thead class="bg-gray-100">
+      <tr>
+        <th colspan="8" class="px-2 py-1 border text-center">Grand Total</th>
+      </tr>
+      <tr>
+        <th class="w-[150px] px-2 py-1 border">Total Saldo Stok</th>
+        <th class="w-[120px] px-2 py-1 border">Total Biaya</th>
+        <th class="w-[120px] px-2 py-1 border">Penjualan</th>
+        <th class="w-[140px] px-2 py-1 border">Discount per Penjualan</th>
+        <th class="w-[160px] px-2 py-1 border">Penjualan Setelah Discount</th>
+        <th class="w-[140px] px-2 py-1 border">Keuntungan Kotor</th>
+        <th class="w-[180px] px-2 py-1 border">Keuntungan Bersih Sebelum Diskon</th>
+        <th class="w-[180px] px-2 py-1 border">Keuntungan Bersih Setelah Diskon</th>
+      </tr>
+    </thead>
     <tbody>
-      <x-table-row class="font-bold bg-gray-50">
-        <x-table-cell class="text-right">{{ $footer['total_ending_stock_balance'] }}</x-table-cell>
-        <x-table-cell class="text-right">{{ $footer['total_cost'] }}</x-table-cell>
-        <x-table-cell class="text-right">{{ $footer['total_gross'] }}</x-table-cell>
-        <x-table-cell class="text-right">{{ $footer['total_discount'] }}</x-table-cell>
-        <x-table-cell class="text-right">{{ $footer['total_net'] }}</x-table-cell>
-        <x-table-cell class="text-right">{{ $footer['total_gross_profit'] }}</x-table-cell>
-        <x-table-cell class="text-right">{{ $footer['total_net_profit_before_discount_selling'] }}</x-table-cell>
-        <x-table-cell class="text-right">{{ $footer['total_net_profit_after_discount_selling'] }}</x-table-cell>
-      </x-table-row>
+      <tr class="font-bold bg-gray-50">
+        <td class="px-2 py-1 border text-right">{{ $footer['total_ending_stock_balance'] }}</td>
+        <td class="px-2 py-1 border text-right">{{ $footer['total_cost'] }}</td>
+        <td class="px-2 py-1 border text-right">{{ $footer['total_gross'] }}</td>
+        <td class="px-2 py-1 border text-right">{{ $footer['total_discount'] }}</td>
+        <td class="px-2 py-1 border text-right">{{ $footer['total_net'] }}</td>
+        <td class="px-2 py-1 border text-right">{{ $footer['total_gross_profit'] }}</td>
+        <td class="px-2 py-1 border text-right">{{ $footer['total_net_profit_before_discount_selling'] }}</td>
+        <td class="px-2 py-1 border text-right">{{ $footer['total_net_profit_after_discount_selling'] }}</td>
+      </tr>
     </tbody>
-  </x-table>
+  </table>
 </div>
